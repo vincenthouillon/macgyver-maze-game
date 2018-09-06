74a35b4222fc2ebc29e4f92c1727033307fd05f4
HVincent <vhouillon@gmail.com>
2018-09-06 17:06:53 +0200

    add perso and move

--- classes.py
+++ classes.py
@@ -3,22 +3,24 @@ from random import choice
 import pygame
 
 from constants import (IMG_ETHER, IMG_GUARDIAN, IMG_MAC, IMG_NEDDLE,
-                       IMG_SPRITES, IMG_TUBE)
+                       IMG_SPRITES, IMG_TUBE, SPRITE_NUMBER, SPRITE_SIZE)
 
 
 class Maze:
     """ Open the text file and generates the structure of the labyrinth :
+
             Maze("filename.txt")
+
         By default, open the original file "labyrinth-scheme.txt"
     """
 
-
-    def __init__(self, txt_file = "labyrinth_scheme.txt"):
+    def __init__(self, txt_file="labyrinth_scheme.txt"):
         self.txt_file = txt_file
         self.maze_structure = []
-        self.maze_with_objects = []
+        self.mac_position = []
         self.__reader_file()
         self.__place_random_objects()
+        self.__get_pos_mac()
 
     def __reader_file(self):
         """ Method for generating the level based on the file.
@@ -57,14 +59,12 @@ class Maze:
     def __place_random_objects(self):
         objects = ["neddle", "tube", "ether"]
         floor = self.__get_floor()
-        empty_maze = self.maze_structure
 
         for i in range(0, len(objects)):
             line_number, index = choice(floor)
-            empty_maze[line_number].pop(index)
-            empty_maze[line_number].insert(index, objects[i])
-        self.maze_with_objects = empty_maze
-        return self.maze_with_objects
+            self.maze_structure[line_number].pop(index)
+            self.maze_structure[line_number].insert(index, objects[i])
+        return self.maze_structure
 
     def display_maze(self, window):
         """ In the text file, the characters correspond to:
@@ -73,41 +73,43 @@ class Maze:
                 > "s" = macgyver (start)
                 > "e" = guardian (end)
         """
+        sprite_dimension = (SPRITE_SIZE, SPRITE_SIZE)
+
         img_wall = pygame.image.load(IMG_SPRITES).convert()
         wall = pygame.transform.scale(
-            img_wall.subsurface(300, 0, 20, 20), (30, 30))
+            img_wall.subsurface(300, 0, 20, 20), (sprite_dimension))
 
         img_floor = pygame.image.load(IMG_SPRITES).convert()
         floor = pygame.transform.scale(
-            img_floor.subsurface(160, 40, 20, 20), (30, 30))
+            img_floor.subsurface(160, 40, 20, 20), (sprite_dimension))
 
-        img_macgyver = pygame.image.load(IMG_MAC).convert_alpha()
-        macgyver = pygame.transform.scale(img_macgyver, (30, 30))
+        # img_macgyver = pygame.image.load(IMG_MAC).convert_alpha()
+        # macgyver = pygame.transform.scale(img_macgyver, (30, 30))
 
         img_guardian = pygame.image.load(IMG_GUARDIAN).convert_alpha()
-        guardian = pygame.transform.scale(img_guardian, (30, 30))
+        guardian = pygame.transform.scale(img_guardian, (sprite_dimension))
 
         img_neddle = pygame.image.load(IMG_NEDDLE).convert_alpha()
-        neddle = pygame.transform.scale(img_neddle, (30, 30))
+        neddle = pygame.transform.scale(img_neddle, (sprite_dimension))
 
         img_tube = pygame.image.load(IMG_TUBE).convert_alpha()
-        tube = pygame.transform.scale(img_tube, (30, 30))
+        tube = pygame.transform.scale(img_tube, (sprite_dimension))
 
         img_ether = pygame.image.load(IMG_ETHER).convert_alpha()
-        ether = pygame.transform.scale(img_ether, (30, 30))
+        ether = pygame.transform.scale(img_ether, (sprite_dimension))
 
         line_number = 0
-        for line in self.maze_with_objects:
+        for line in self.maze_structure:
             case_number = 0
             for sprite in line:
-                pos_x = case_number * 30
-                pos_y = line_number * 30
+                pos_x = case_number * SPRITE_SIZE
+                pos_y = line_number * SPRITE_SIZE
                 if sprite == " ":
                     window.blit(floor, (pos_x, pos_y))
                 elif sprite == "w":
                     window.blit(wall, (pos_x, pos_y))
                 elif sprite == "s":
-                    window.blit(macgyver, (pos_x, pos_y))
+                    window.blit(floor, (pos_x, pos_y))
                 elif sprite == "e":
                     window.blit(guardian, (pos_x, pos_y))
                 elif sprite == "neddle":
@@ -119,9 +121,65 @@ class Maze:
                 case_number += 1
             line_number += 1
 
+    def __get_pos_mac(self):
+        """ Get the postition of Mac Gyver
+        """
+        line_number = 0
+        for line in self.maze_structure:
+            case_number = 0
+            for sprite in line:
+                pos_x = case_number
+                pos_y = line_number
+                if sprite == "s":
+                    self.mac_position = [pos_x, pos_y]
+                case_number += 1
+            line_number += 1
+
+
+class MacGyver:
+    def __init__(self, labyrinth):
+        self.labyrinth = labyrinth
+        img_macgyver = pygame.image.load(IMG_MAC).convert_alpha()
+        self.img_mac = pygame.transform.scale(
+            img_macgyver, (SPRITE_SIZE, SPRITE_SIZE))
+        # * Initial position
+        self.case_x, self.case_y = labyrinth.mac_position
+        self.x = self.case_x * SPRITE_SIZE
+        self.y = self.case_y * SPRITE_SIZE
+        print(f"Position de mac : {self.labyrinth.mac_position}")
+
+    def move(self, direction):
+        # Move to right
+        if direction == "right":
+            if self.labyrinth.maze_structure[self.case_y][self.case_x + 1] != "w":
+                self.case_x += 1
+                self.x = self.case_x * SPRITE_SIZE
+                print(f"RIGHT: case_x = {self.case_x}, x = {self.x}")
+
+        # Move to left
+        if direction == "left":
+            if self.labyrinth.maze_structure[self.case_y][self.case_x - 1] != "w":
+                self.case_x -= 1
+                self.x = self.case_x * SPRITE_SIZE
+                print(f"LEFT: case_x = {self.case_x}, x = {self.x}")
+
+        # Move to up
+        if direction == "up":
+            if self.labyrinth.maze_structure[self.case_y - 1][self.case_x] != "w":
+                self.case_y -= 1
+                self.y = self.case_y * SPRITE_SIZE
+                print(f"UP: case_y = {self.case_y}, y = {self.y}")
+
+        # Move to down
+        if direction == "down":
+            if self.labyrinth.maze_structure[self.case_y + 1][self.case_x] != "w":
+                self.case_y += 1
+                self.y = self.case_y * SPRITE_SIZE
+                print(f"DOWN: case_y = {self.case_y}, y = {self.y}")
+
 
 def main():
-    labyrinth = Maze() 
+    labyrinth = Maze()
     print("Maze 15x15 with random objects:\n===============================")
     for line in labyrinth.maze_structure:
         print(line)
--- macgyver-maze-game.py
+++ macgyver-maze-game.py
@@ -7,8 +7,8 @@ Game in which we must move Mac Gyver to escape a labyrinth.
 
 import pygame
 
-from classes import Maze
-from constants import DISPLAY_SIZE, ICON_GAME, TITLE_WINDOW
+from classes import Maze, MacGyver
+from constants import DISPLAY_SIZE, ICON_GAME, TITLE_WINDOW, IMG_MAC
 
 pygame.init()
 
@@ -20,22 +20,35 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption(TITLE_WINDOW)
 pygame.display.set_icon(icon)
+pygame.key.set_repeat(200, 300)
 screen.fill(bg_color)
 
 labyrinth = Maze()
-labyrinth.display_maze(screen)
+macgyver = MacGyver(labyrinth)
 
 game_continue = True
 
 while game_continue:
 
+    clock.tick(30)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and \
                 event.key == pygame.K_ESCAPE:
             game_continue = False
 
-    pygame.display.update()
-    clock.tick(30)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                macgyver.move("right")
+            elif event.key == pygame.K_LEFT:
+                macgyver.move("left")
+            if event.key == pygame.K_UP:
+                macgyver.move("up")
+            if event.key == pygame.K_DOWN:
+                macgyver.move("down")
 
+        labyrinth.display_maze(screen)
+        screen.blit(macgyver.img_mac, (macgyver.x, macgyver.y))
+        pygame.display.flip()
 
 pygame.quit()
